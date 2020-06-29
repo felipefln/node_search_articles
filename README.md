0ec8d09d3234d2f5688ef5acee4382bfae83a963
Felipe Weiduschadt de Carvalho <felipe.carvalhofln@gmail.com>
2020-06-29 00:22:08 -0300

    Feat: Criado rota para realizar um get no ID do artigo.

--- README.md
+++ README.md
@@ -8,14 +8,15 @@ API NODEJS
 - Backend
   - Node.js
   - Express
+  - Axios
   - \* [check package.json](/package.json)
 
 **Example run Backend:**
 
 1. Clone your repository `https://github.com/felipefln/node_search_articles.git`
 2. Go to the dir: `cd node_search_articles`
-3. Install dependen: `yarn install` || `npm install`
-4. Run the script: `yarn dev` || `npm start`
+3. Install dependen: `yarn install`
+4. Run the script: `yarn start`
 5. Now it's running on [http://localhost:4000](http://localhost:4000)
 
 ## More info
--- routes/search.js
+++ routes/search.js
@@ -2,12 +2,33 @@ const express = require("express");
 const router = express.Router();
 const fetch = require("node-fetch");
 
+// rota post para buscar o palavra enviada no POST
 router.post("/", async (req, res) => {
-  if (req.body.searchInput !== "") {
-    const data = await fetch(`https://core.ac.uk:443/api-v2/articles/search/${req.body.searchInput}?page=${req.body.indexPage}&pageSize=10&metadata=true&fulltext=true&citations=false&similar=false&duplicate=false&urls=false&faithfulMetadata=false&apiKey=${process.env.CORE_API_KEY}
+  const { searchInput } = req.body;
+  if (searchInput !== "") {
+    try {
+      const data = await fetch(`https://core.ac.uk:443/api-v2/articles/search/${searchInput}?page=${req.body.indexPage}&pageSize=10&metadata=true&fulltext=true&citations=false&similar=false&duplicate=false&urls=false&faithfulMetadata=false&apiKey=${process.env.CORE_API_KEY}
         `);
-    const items = await data.json();
-    await res.status(200).send(items);
+      const items = await data.json();
+      await res.status(200).send(items);
+    } catch {
+      console.error();
+    }
+  }
+});
+
+// rota para buscar o ID de determinado artigo
+router.get("/", async (req, res) => {
+  const { id: coreId } = req.query;
+  if (coreId !== "") {
+    try {
+      const data = await fetch(`https://core.ac.uk:443/api-v2/articles/get/${coreId}?metadata=true&fulltext=true&citations=false&similar=false&duplicate=false&urls=false&faithfulMetadata=false&apiKey=${process.env.CORE_API_KEY}
+        `);
+      const items = await data.json();
+      await res.status(200).send(items);
+    } catch {
+      console.error();
+    }
   }
 });
 
